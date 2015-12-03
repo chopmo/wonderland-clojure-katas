cf4440e89c56058a67fd90abb14c764b4343a1c5
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2015-12-03 08:38:31 +0000

    Hardcoded boat solution

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -3,4 +3,20 @@
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
 (defn river-crossing-plan []
-  start-pos)
+  [[[:fox :goose :corn :you] [:boat] []]
+   [[:fox :corn] [:boat :goose :you] []]
+   [[:fox :corn] [:boat] [:goose :you]]
+   [[:fox :corn] [:boat :you] [:goose]]
+   [[:fox :corn :you] [:boat] [:goose]]
+   [[:fox] [:boat :corn :you] [:goose]]
+   [[:fox] [:boat] [:goose :corn :you]]
+   [[:fox] [:boat :you :goose] [:corn]]
+   [[:fox :goose :you] [:boat] [:corn]]
+   [[:goose] [:boat :fox :you] [:corn]]
+   [[:goose] [:boat] [:corn :fox :you]]
+   [[:goose] [:boat :you] [:corn :fox]]
+   [[:goose :you] [:boat] [:corn :fox]]
+   [[] [:boat :goose :you] [:corn :fox]]
+   [[] [:boat] [:fox :goose :corn :you]]
+   ]
+  )
