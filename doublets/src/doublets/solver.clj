0c6d948784c1c29c3fbca43349e66afd870d821b
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2016-06-18 22:19:27 +0200

    Solve doublet kata
    I am quite happy with this solution which uses tree-seq.

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -1,11 +1,36 @@
 (ns doublets.solver
   (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clojure.set :as set]))
 
 (def words (-> "words.edn"
                (io/resource)
                (slurp)
-               (read-string)))
+               (read-string)
+               (set)))
 
 (defn doublets [word1 word2]
-  "make me work")
+  (vec (first (solutions word1 word2))))
+
+(defn solutions [w1 w2]
+  (filter #(= w2 (last %)) (chains w1)))
+
+(defn chains [w]
+  (tree-seq branch? children [w]))
+
+;; Helper functions for building the tree
+(defn children [ws]
+  (let [w (last ws)
+        unused-words (set/difference words (set ws))
+        neighbours (filter #(neighbours? w %) unused-words)]
+    (map #(conj ws %) neighbours)))
+
+(defn branch? [ws]
+  (seq (children ws)))
+
+(defn neighbours? [w1 w2]
+  (let [pairs (map vector w1 w2)
+        diffs (filter #(not (apply = %)) pairs)]
+    (and
+     (= 1 (count diffs))
+     (= (count w1) (count w2)))))
