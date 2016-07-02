acce9f9f58651a586f8e0d8790f6ea367bfbcefc
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2016-07-02 23:15:21 +0200

    Implement tiny maze solver
    Pretty simple implementation for now. I have tried to make the
    implementation as readable as possible rather than fancy.
    
    I have a few ideas for improvements that I will probably apply in later
    commits.
    
    I was really happy with this kata. It felt like something clicked - I
    thought carefully about how to solve it, then the actual implementation
    was actually quite easy.

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,31 @@
-(ns tiny-maze.solver)
+(ns tiny-maze.solver
+  (:require [clojure.pprint :refer [pprint]]))
 
-(defn solve-maze [maze])
+(defn solve-maze [maze]
+  (let [path (find-path maze [] [0 0])]
+    (render-path maze path)))
+
+(defn find-path [maze path position]
+  (let [new-path (conj path position)]
+    (case (get-in maze position)
+      :E new-path
+      (:S 0) (->> (neighbours position)
+                  (filter-unvisited path)
+                  (map #(find-path maze new-path %))
+                  (filter identity)
+                  (first))
+      nil)))
+
+(defn neighbours [[row col]]
+  [[(dec row) col]
+   [(inc row) col]
+   [row (dec col)]
+   [row (inc col)]])
+
+(defn filter-unvisited [path positions]
+  (let [unvisited? (fn [position]
+                     (not ((set path) position)))]
+    (filter unvisited? positions)))
+
+(defn render-path [maze path]
+  (reduce #(assoc-in %1 %2 :x) maze path))
