203f9d6c401289b33a9e0c1fb8b9056e0f324fb6
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2016-07-16 16:17:21 +0200

    Completed game of war
    Functionally complete, but the code is a mess which I will now attempt
    to refactor.
    
    I have stepped through the test case where each player starts with 3
    cards, and it seems to work correctly.
    
    However, I'm a bit surprised to see that when trying a few games with
    random cards, the algorithm seems to very easily enter a loop and never
    terminates. I'll investigate Carol's solution when I'm happy with my
    own.

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,6 +1,5 @@
 (ns card-game-war.game)
 
-;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
 (def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
 (def cards
@@ -23,4 +22,15 @@
      :player2
      :player1)))
 
-(defn play-game [player1-cards player2-cards])
+(defn play-game [player1-cards player2-cards]
+  (if (empty? player1-cards)
+    :player2
+    (if (empty? player2-cards)
+      :player1
+      (let [p1-card (first player1-cards)
+            p1-hand (vec (rest player1-cards))
+            p2-card (first player2-cards)
+            p2-hand (vec (rest player2-cards))]
+        (if (= :player1 (play-round p1-card p2-card))
+          (recur (conj p1-hand p1-card p2-card) p2-hand)
+          (recur p1-hand (conj p2-hand p1-card p2-card)))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -3,7 +3,6 @@
             [card-game-war.game :refer :all]))
 
 
-;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
     (is (= :player1
@@ -18,11 +17,43 @@
            (play-round [:spade :queen]
                        [:club :jack]))))
 
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
+  (testing "kings are higher rank than queens"
+    (is (= :player2
+           (play-round [:spade :queen]
+                       [:spade :king]))))
+
+  (testing "aces are higher rank than kings"
+    (is (= :player1
+           (play-round [:spade :ace]
+                       [:spade :king]))))
+
   (testing "if the ranks are equal, clubs beat spades")
   (testing "if the ranks are equal, diamonds beat clubs")
   (testing "if the ranks are equal, hearts beat diamonds"))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (is (= :player1
+           (play-game
+            [[:spade 2]]
+            [])))
+    (is (= :player2
+           (play-game
+            []
+            [[:spade 2]]))))
+
+  (testing "multiple rounds are played"
+    (is (= :player1
+           (play-game
+            [[:spade 3]]
+            [[:spade 2]]))))
+
+  (testing "the best deck wins"
+    (is (= :player1
+           (play-game
+            [[:spade 10]
+             [:spade 3]
+             [:spade 8]]
+            [[:spade 2]
+             [:spade 4]
+             [:spade 5]])))))
